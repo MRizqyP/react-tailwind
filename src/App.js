4ba81595b8af7b3d85011622d6482c07831b7966
Mohammad Rizqy Pratama <mohammad.rizqypratama@gmail.com>
2021-02-03 10:51:51 +0700

    Setting Up Taildwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Kinyot ga tuh <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React tapi boong
-        </a>
-      </header>
+      <h1 className="text-red-500">YOOO</h1>
     </div>
   );
 }
