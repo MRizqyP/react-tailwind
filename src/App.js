b3f60031eb1ca509060159e776267f060404d045
Mohammad Rizqy Pratama <mohammad.rizqypratama@gmail.com>
2021-02-03 14:57:09 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Content from './components/Content';
+import Footer from './components/Footer';
+import Home from './pages';
+import About from './pages/about';
+import Menu from './pages/menu';
+import Navbar from './components/Navbar';
+import { Switch, Route } from 'react-router-dom';
+import Dropdown from './components/Dropdown';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+        console.log('i resized');
+      }
+    };
+
+    window.addEventListener('resize', hideMenu);
+
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    };
+  });
+
   return (
-    <div className="App">
-      <h1 className="text-red-500">YOOO</h1>
-    </div>
+    <>
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' exact component={About} />
+        <Route path='/menu' exact component={Menu} />
+      </Switch>
+      <Footer />
+    </>
   );
 }
 
